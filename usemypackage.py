1f93e57b40f95a93558766d6518cb8b0282292ed
sichyi <qizi86@hotmail.com>
2024-07-17 10:01:42 +0800

    Delete usemypackage.py

--- usemypackage.py
+++
@@ -1,9 +0,0 @@
-# ===============
-# usemypackage.py
-# ===============
-import mypackage.module1
-import mypackage.module2
-mypackage.module1_function1() # module1_function1
-mypackage.module1_function2() # module1_function2
-mypackage.module2_function1() # module2_function1
-mypackage.module2_function2() # module2_function2
